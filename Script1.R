02e87b6abbff24c2dc590149123161f7f724a674
GiuseppePennisi <giuseppe.pennisi11@gmail.com>
2017-12-22 17:19:41 +0100

    I have computed total population of city.

--- Script1.R
+++ Script1.R
@@ -1,4 +1,5 @@
-mydata = read.csv("2010_Census_Populations_by_Zip_Code.csv")
-options(max.print = .Machine$integer.max)
-print(mydata)
-print("Hello Giuseppe!")
+cityCensus = read.csv("2010_Census_Populations_by_Zip_Code.csv")
+options(max.print = .Machine$integer.max) #let to print all dataframe's rows without omit anyone
+
+totPopulation=sum(mydata$Total.Population) # computation of the total city's population 
+print(paste("The total population is:" ,totPopulation))
