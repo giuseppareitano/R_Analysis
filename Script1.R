aae83096775da0cc50554c60eb029b5d8ae706c9
giuseppa.reitano <giuseppareitano@gmail.com>
2017-12-23 16:21:12 +0100

    Extract a subset from dataframe!

--- Script1.R
+++ Script1.R
@@ -4,12 +4,15 @@ options(max.print = .Machine$integer.max) #let to print all dataframe's rows wit
 totPopulation=sum(cityCensus$Total.Population) # computation of the total city's population
 print(paste("The total population is:" ,totPopulation))
 
-
+#this is the function to apply to all dataframe
+#it extracts a list of triples that contain the ZipCode, the male percent and the female percent
 percentMaleFemale = function(x){
   lista = c(x[1], x[4]/x[2]*100, x[5]/x[2]*100)
 }
-matrix = as.matrix(t(apply(cityCensus,1,percentMaleFemale)))
-dimnames(matrix)[[2]]<-c("ZipCode","MalesPercent","FemalesPercent")
+matrix = as.matrix(t(apply(cityCensus,1,percentMaleFemale))) #conversion from list to matrix
+dimnames(matrix)[[2]]<-c("ZipCode","MalesPercent","FemalesPercent") #renaming of matrix columns
 print(matrix)
-print(subset(cityCensus,subset = (Average.Household.Size>3),select = c(Zip.Code, Total.Population)))
 
+#Now extract a subset of dataframe compound by ZipCode and Total Population of all districts with Average Household Size >3
+subCensus = subset(cityCensus,subset = (Average.Household.Size>3),select = c(Zip.Code, Total.Population))
+write.table(subCensus,"Output3.txt",sep="\t",row.names=FALSE) #Saving subset into a file Output3.txt
